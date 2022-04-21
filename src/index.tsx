6ade58d4487fed8132ed663e443312641bbfbc98
lifeng <506076395@qq.com>
2022-04-22 06:38:00 +0800

    Update index.html

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+    // StrictMode 该标签无实际用途 就是检查代码用错的地方 例如不推荐的 API 过时的代码等
   <React.StrictMode>
     <App />
   </React.StrictMode>
