298722ceb9c5f6dd9152b3753e8d2a2da23d49ca
lifeng <506076395@qq.com>
2022-04-23 16:46:41 +0800

    滚回5版本 router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,54 @@
 import React from 'react';
 import {
-  BrowserRouter,
-  Routes,
+  HashRouter as Router,
+  Switch,
   Route,
-  Link,
+  Link, Redirect
 } from 'react-router-dom';
-
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
         <nav>
           <ul>
             <li>
-              <Link to="/money">记账页面</Link>
+              <Link to="/tags">标签页</Link>
             </li>
             <li>
-              <Link to="/tags">标签页</Link>
+              <Link to="/money">记账页</Link>
             </li>
             <li>
               <Link to="/statistics">统计页</Link>
             </li>
           </ul>
         </nav>
-        <Routes>
-          <Route path="/money" element={<Money/>}/>
-          <Route path="/tags" element={<Tags/>}/>
-          <Route path="/statistics" element={<Statistics/>}/>
-        </Routes>
+
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          {/*默认路由*/}
+          <Redirect exact from="/" to="/money"/>
+          {/*404 路由*/}
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>请输入正确的网址</div>
   );
 }
 
@@ -38,12 +56,12 @@ function Statistics() {
   return <h2>统计页面</h2>;
 }
 
-function Money() {
-  return <h2>记账页面</h2>;
-}
-
 function Tags() {
   return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
 }
 
 export default App;
